543b72719e7f1124514cc5d63aec306632a1e61d
silviacremascoli <silcremascoli@gmail.com>
2023-09-08 12:29:44 +0200

    Added forEach loop for skills, tools and learning

--- work.js
+++ work.js
@@ -1,100 +1,193 @@
 const projects = [
-    {
-        name: "World Clock Project",
-        img: "world-clock-home",
-        alt: "world clock project",
-        id: "world-clock-project",
-        "built-with": "HTML, CSS and JavaScript",
-        "aos-flip": "flip-right",
-        github: "https://github.com/silviacremascoli/world-clock",
-        link: "https://my-world-clock-app-silvia-cremascoli.netlify.app/"
-    },
-    {
-        name: "Weather Project",
-        img: "weather-home",
-        alt: "weather project",
-        id: "weather-project",
-        "built-with": "HTML, CSS and React.js",
-        "aos-flip": "flip-left",
-        github: "https://github.com/silviacremascoli/react-weather-app",
-        link: "https://react-weather-app-silvia-cremascoli.netlify.app/"
-    },
-    {
-        name: "Travel Page Project",
-        img: "travel-home",
-        alt: "travel page project",
-        id: "travel-page-project",
-        "built-with": "HTML and CSS",
-        "aos-flip": "flip-right",
-        github: "https://github.com/silviacremascoli/travel-page-bali",
-        link: "https://travel-page-bali-silvia-cremascoli.netlify.app/"
-    },
-    {
-        name: "Dictionary Project",
-        img: "dictionary-home",
-        alt: "dictionary project",
-        id: "dictionary-project",
-        "built-with": "HTML, CSS and React.js",
-        "aos-flip": "flip-left",
-        github: "https://github.com/silviacremascoli/react-dictionary-app",
-        link: "https://react-dictionary-silvia-cremascoli.netlify.app/"
-    },
-    {
-        name: "Portfolio Project",
-        img: "portfolio-home",
-        alt: "portfolio project",
-        id: "portfolio-project",
-        "built-with": "HTML, CSS and Vue.js",
-        "aos-flip": "flip-right",
-        github: "https://github.com/silviacremascoli/nandor-bottyan-portfolio",
-        link: "https://vue-js-personal-portfolio.netlify.app/"
-    },
-    {
-        name: "Restaurant Project",
-        img: "restaurant-home",
-        alt: "restaurant project",
-        id: "restaurant-project",
-        "built-with": "HTML, CSS and PHP",
-        "aos-flip": "flip-left",
-        github: "https://github.com/silviacremascoli/restaurant_dynamic_website",
-        link: "http://restaurantfrank.byethost32.com/"
-    }
-]
-
-/* const skillCategories = ["My Skills"
-]
+  {
+    name: "World Clock Project",
+    img: "world-clock-home",
+    alt: "world clock project",
+    id: "world-clock-project",
+    "built-with": "HTML, CSS and JavaScript",
+    "aos-flip": "flip-right",
+    github: "https://github.com/silviacremascoli/world-clock",
+    link: "https://my-world-clock-app-silvia-cremascoli.netlify.app/",
+  },
+  {
+    name: "Weather Project",
+    img: "weather-home",
+    alt: "weather project",
+    id: "weather-project",
+    "built-with": "HTML, CSS and React.js",
+    "aos-flip": "flip-left",
+    github: "https://github.com/silviacremascoli/react-weather-app",
+    link: "https://react-weather-app-silvia-cremascoli.netlify.app/",
+  },
+  {
+    name: "Travel Page Project",
+    img: "travel-home",
+    alt: "travel page project",
+    id: "travel-page-project",
+    "built-with": "HTML and CSS",
+    "aos-flip": "flip-right",
+    github: "https://github.com/silviacremascoli/travel-page-bali",
+    link: "https://travel-page-bali-silvia-cremascoli.netlify.app/",
+  },
+  {
+    name: "Dictionary Project",
+    img: "dictionary-home",
+    alt: "dictionary project",
+    id: "dictionary-project",
+    "built-with": "HTML, CSS and React.js",
+    "aos-flip": "flip-left",
+    github: "https://github.com/silviacremascoli/react-dictionary-app",
+    link: "https://react-dictionary-silvia-cremascoli.netlify.app/",
+  },
+  {
+    name: "Portfolio Project",
+    img: "portfolio-home",
+    alt: "portfolio project",
+    id: "portfolio-project",
+    "built-with": "HTML, CSS and Vue.js",
+    "aos-flip": "flip-right",
+    github: "https://github.com/silviacremascoli/nandor-bottyan-portfolio",
+    link: "https://vue-js-personal-portfolio.netlify.app/",
+  },
+  {
+    name: "Restaurant Project",
+    img: "restaurant-home",
+    alt: "restaurant project",
+    id: "restaurant-project",
+    "built-with": "HTML, CSS and PHP",
+    "aos-flip": "flip-left",
+    github: "https://github.com/silviacremascoli/restaurant_dynamic_website",
+    link: "http://restaurantfrank.byethost32.com/",
+  },
+];
 
 const skills = [
+  {
+    name: "HTML",
+    class: "fa-brands fa-html5",
+  },
+  {
+    name: "CSS",
+    class: "fa-brands fa-css3-alt",
+  },
+  {
+    name: "SASS",
+    class: "fa-brands fa-sass",
+  },
+  {
+    name: "BOOTSTRAP",
+    class: "fa-brands fa-bootstrap",
+  },
+  {
+    name: "JAVASCRIPT",
+    class: "fa-brands fa-square-js",
+  },
+  {
+    name: "API",
+    class: "fa-solid fa-gear",
+  },
+  {
+    name: "REACT.JS",
+    class: "fa-brands fa-react",
+  },
+  {
+    name: "VUE.JS",
+    class: "fa-brands fa-vuejs",
+  },
+  {
+    name: "PHP",
+    class: "fa-brands fa-php",
+  },
+];
 
-    {
-        category: {
-            "category-name": "My skills": {
-    {
-        name: "HTML",
-        "i-class": "fa-brands fa-html5"
-    },
-    {
-        name: "CSS",
-        "i-class": "fa-brands fa-css3-alt"
-    },
-    {
-        name: "SASS",
-        "i-class": "fa-brands fa-sass"
-    }
-            }
-        }
+const tools = [
+  {
+    name: "VISUAL STUDIO CODE",
+    class: "fa-solid fa-code",
+  },
+  {
+    name: "PHPSTORM",
+    class: "fa-solid fa-laptop-code",
+  },
+  {
+    name: "GITHUB",
+    class: "fa-brands fa-github",
+  },
+  {
+    name: "FIGMA",
+    class: "fa-brands fa-figma",
+  },
+  {
+    name: "CANVA",
+    class: "fa-solid fa-pencil",
+  },
+  {
+    name: "WORDPRESS",
+    class: "fa-brands fa-wordpress",
+  },
+  {
+    name: "JIRA",
+    class: "fa-brands fa-jira",
+  },
+  {
+    name: "CONFLUENCE",
+    class: "fa-brands fa-confluence",
+  },
+];
 
-    }
-] */
+const learning = [
+  {
+    name: "UNIT TESTING",
+    class: "fa-solid fa-wrench",
+  },
+  {
+    name: "TYPESCRIPT",
+    class: "fa-brands fa-windows",
+  },
+  {
+    name: "NEXT.JS",
+    class: "fa-brands fa-react",
+  },
+];
+
+function displaySkill() {
+  const skill = document.getElementById("my-skills");
+  let skillHTML = `<div>`;
+  skills.forEach((mySkill) => {
+    skillHTML = skillHTML + `<p><i class="${mySkill.class}"></i> ${mySkill.name}</p>`;
+  });
+  skillHTML = skillHTML + `</div>`;
+  skill.innerHTML = skillHTML;
+}
+
+function displayTool() {
+  const tool = document.getElementById("my-tools");
+  let toolHTML = `<div>`;
+  tools.forEach((myTool) => {
+    toolHTML = toolHTML + `<p><i class="${myTool.class}"></i> ${myTool.name}</p>`;
+  });
+  toolHTML = toolHTML + `</div>`;
+  tool.innerHTML = toolHTML;
+}
+
+function displayLearning() {
+  const learningDiv = document.getElementById("learning");
+  let learningHTML = `<div>`;
+  learning.forEach((topic) => {
+    learningHTML = learningHTML + `<p><i class="${topic.class}"></i> ${topic.name}</p>`;
+  });
+  learningHTML = learningHTML + `</div>`;
+  learningDiv.innerHTML = learningHTML;
+}
 
 function displayProject() {
-    const project = document.getElementById("project");
+  const project = document.getElementById("project");
 
-    let projectHTML = `<div class="row">`;
-    projects.forEach(function (myProject) {
-        projectHTML =
-                projectHTML +
-                `
+  let projectHTML = `<div class="row">`;
+  projects.forEach((myProject) => {
+    projectHTML =
+      projectHTML +
+      `
     <div class="col-md-6 mb-5">
     <img
         src="/images/${myProject.img}.png"
@@ -124,10 +217,14 @@ function displayProject() {
     >
     </div>
     `;
-    })
-    projectHTML = projectHTML + `</div>`;
-    project.innerHTML = projectHTML;
+  });
+  projectHTML = projectHTML + `</div>`;
+  project.innerHTML = projectHTML;
+  AOS.init();
 }
 
-displayProject();
 AOS.init();
+displayProject();
+displaySkill();
+displayTool();
+displayLearning();
